db0d584f27bbbb2bd2f1cb6349f43d8a031d4ebf
karl-police <karl-police2001@bluewin.ch>
2023-09-30 23:10:19 +0200

    Update entityclass.cpp

--- entity2/entityclass.cpp
+++ entity2/entityclass.cpp
@@ -4,7 +4,7 @@
 typedef long __int64;
 
 // If it's really __int64 for the function is the other question
-__int64 CEntityClass::Unserialize() {
+__int64 CEntityClass::Unserialize(/*__int64 a1, __int64 a2, _QWORD *a3*/) {
   __int64 entity_index; // v8, e.g. ent_attachments entity_index
   const char* entity_className; // v7, e.g. weapon_glock
 
